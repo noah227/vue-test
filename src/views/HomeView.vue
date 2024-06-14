46d4ca80f8277861929f4c51b24e87286032682c
noah227 <noah227@foxmail.com>
2024-06-14 11:12:25 +0800

    chore: 稍稍调整主页，添加about的跳转

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+        <router-link to="/about">Go About</router-link>
     </div>
 </template>
 
