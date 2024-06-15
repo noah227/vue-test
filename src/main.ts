3722c06df1442c642cadc0ad72047cd7d5d4045f
NoahYoung <noah227@foxmail.com>
2024-06-15 16:51:45 +0800

    test: 添加console.image的测试
    限制只能渲染/public下的图片，其他路径（如@/*）下的图片暂时无法获取

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import console from "@cynario/console.image"
+
+console.image("/test.png", {width: 256})
 
 createApp(App).use(store).use(router).mount('#app')
